cc378364c2d969dea79c6822a75d357e51bd67ce
Ezgi Tastan <kedigunesi@gmail.com>
2024-02-20 21:42:24 +0300

    Create new.py

---
+++ new.py
@@ -0,0 +1,20 @@
+class ValueTooHighError(Exception):
+    pass
+
+class ValueTooLowError(Exception):
+    def __init__(self, message, value):
+        self.message = message
+        self.value = value
+
+def test_value(x):
+    if x > 100:
+        raise ValueTooHighError("value is too high")
+    if x < 5:
+        raise ValueTooLowError("Value is too low.", x)  # value parametresi sona ekleniyor
+
+try:
+    test_value(101)
+except ValueTooHighError as e:
+    print(e)
+except ValueTooLowError as e:
+    print(e.message, e.value)
